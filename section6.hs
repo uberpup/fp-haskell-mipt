0d904900e57f9c9b4df6ac57bf851bc47215d49d
Vladimir Tochilin <tochilin.vn@phystech.edu>
2021-04-22 20:21:52 +0300

    Task 33

--- section6.hs
+++ section6.hs
@@ -29,10 +29,11 @@ dropWhile' p (x:xs) | p x = dropWhile' p xs
 					| otherwise = x : xs 
 -- task 30
 unzip' :: [(a, b)] -> ([a], [b])
-unzip' =
+unzip' [] = ([], [])
+unzip' xs = (map fst xs, map snd xs)
 
 unzip'' :: [(a, b)] -> ([a], [b])
-unzip'' = 
+unzip'' = foldr (\(x,y) ~(xs, ys) -> (x:xs, y:ys)) ([], [])
 
 -- task 31
 
@@ -44,5 +45,17 @@ curry' f = \x y -> f (x, y)
 
 uncurry' :: (x -> y -> z) -> ((x, y) -> z)
 uncurry' f = \(x, y) -> f x y
+
 -- task 33
+zapp :: [a -> b] -> [a] -> [b]
+zapp _ [] = []
+zapp [] _ = []
+zapp (f:fs) (x:xs) = (f $ x) : zapp (fs) (xs)
+
 -- task 34
+-- [x,x,y,z,z,z] -> [(x, 2), (y, 1), (z, 3)]
+compressList :: Eq a => [a] -> [a]
+compressList x = foldr (\x xs -> if x == (head xs) then xs else x:xs) [last x] x
+
+--decompressList :: [(a, Int)] -> [a]
+--decompressList =
