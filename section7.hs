c5cd1cf620da02fe3b5ad0830cf5ce539f6cff95
Vladimir Tochilin <tochilin.vn@phystech.edu>
2021-04-23 19:59:42 +0300

    Task 35

--- section7.hs
+++ section7.hs
@@ -1,11 +1,20 @@
--- task 35
-fib :: Nat -> Nat
-fib 0 = 0
-fib 1 = 1
-fib k = fib(k - 1) + fib(k - 2)
+data Nat = Z | S Nat
+        deriving (Eq,Ord,Show)
 
 foldn :: (a -> a) -> a -> Nat -> a
 foldn f x Z = x
 foldn f x (S n) = f (foldn f x n)
 
-fib' :: Nat -> Nat
+
+nadd :: Nat -> Nat -> Nat
+nadd m Z  = m
+nadd m (S n) = S (nadd n m)
+
+-- task 35
+fib :: Nat -> Nat
+fib Z = Z
+fib (S Z) = (S Z)
+fib k = nadd(fib(k - 1), fib(k - 2))
+
+--fib' :: Nat -> Nat
+--fib' k = foldn 
