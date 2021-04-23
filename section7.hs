4acfafa047e7f76d119ff7896f57b105f5f04fa9
Vladimir Tochilin <tochilin.vn@phystech.edu>
2021-04-23 20:15:53 +0300

    Task 35

--- section7.hs
+++ section7.hs
@@ -10,11 +10,13 @@ nadd :: Nat -> Nat -> Nat
 nadd m Z  = m
 nadd m (S n) = S (nadd n m)
 
+npred = snd . foldn (\(n,x) -> (S n, n)) (Z,Z)
+
 -- task 35
 fib :: Nat -> Nat
 fib Z = Z
 fib (S Z) = (S Z)
-fib k = nadd(fib(k - 1), fib(k - 2))
+fib (S (S k))   = nadd (fib k) (fib(S k))
 
 --fib' :: Nat -> Nat
 --fib' k = foldn 
